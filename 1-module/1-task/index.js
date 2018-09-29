2138afa6874194b753c6903849bb457c221523b6
rh77 <rhawaiian@gmail.com>
2018-09-29 16:46:39 +0300

    task 1-1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -6,4 +6,27 @@
  */
 function pow (m, n) {
 
+	n = parseFloat(n);
+
+	if (!isNumeric(n)) return;
+	if (!isNatural(n)) return;
+
+	let result = m;
+
+	for (let i = 2; i <= n; i++)
+	{
+		result *= m;
+	}
+
+	return result;
+}
+
+function isNatural(value)
+{
+	return value > 0 && (~~value) == value;
+}
+
+function isNumeric(value) {
+
+	return !isNaN(value) && isFinite(value);
 }
