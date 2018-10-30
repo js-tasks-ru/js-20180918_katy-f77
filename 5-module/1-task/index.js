318a1ee4f6cbde334adbc2302ee73223de076932
rh77 <rhawaiian@gmail.com>
2018-10-30 19:53:47 +0300

    task 5-1

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -5,4 +5,35 @@
  * @param {Element} table
  */
 function highlight(table) {
+
+    let rows = table.querySelectorAll("tbody tr");
+
+    for (let i = 0; i < rows.length; i++ )
+    {
+        let row = rows[i];
+        let status = row.lastElementChild;
+        let gender = status.previousElementSibling;
+        let age = gender.previousElementSibling;
+        
+        const dataAvailableAttributeName = "data-available";
+
+        if (status.hasAttribute(dataAvailableAttributeName))
+        {
+            let dataAvailable = status.getAttribute(dataAvailableAttributeName).toLowerCase() == "true";
+            row.classList.toggle("available", dataAvailable);
+            row.classList.toggle("unavailable", !dataAvailable);
+        }
+        else
+        {
+            row.setAttribute("hidden", true);
+        }
+
+        row.classList.toggle("male", gender.textContent == "m");
+        row.classList.toggle("female", gender.textContent == "f");
+
+        if (parseInt(age.textContent) < 18)
+        {
+            row.style.textDecoration = "line-through";
+        }
+    }
 }
